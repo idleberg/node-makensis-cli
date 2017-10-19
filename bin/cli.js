c2ed2072348944de442d2672651093729ed09d02
Jan T. Sott <jan@idleberg.com>
2017-10-19 23:19:00 +0200

    one-file

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,12 @@
+#!/usr/bin/env node
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.version = exports.hdrinfo = exports.compile = exports.cmdhelp = undefined;
-
-var _makensis = require('makensis');
-
-var makensis = _interopRequireWildcard(_makensis);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
+var makensis = require('makensis');
+var meta = require('../package.json');
+var program = require('commander');
 var YAML = require('yamljs');
 
+// Functions
 var compile = function compile(filePath) {
   var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
 
@@ -209,7 +203,73 @@ var isInteger = function isInteger(x) {
   return x % 2 === 0;
 };
 
-exports.cmdhelp = cmdhelp;
-exports.compile = compile;
-exports.hdrinfo = hdrinfo;
-exports.version = version;
+var validCharsets = ['ACP', 'OEM', 'UTF8', 'UTF16BE', 'UTF16LE'];
+
+// Action
+program.version(meta.version).description('CLI version of node-makensis').arguments('<command> [file.nsi]>').usage('<command> [file.nsi] [options]').option('-i, --input-charset <string>', 'ACP|OEM|CP#|UTF8|UTF16[LE|BE]').option('-j, --json', 'prints output as JSON').option('-p, --pause', 'pauses after execution').option('-P, --ppo', 'preprocess to stdout/file').option('-S, --safe-ppo', 'preprocess to stdout/file').option('-v, --verbose <n>', 'verbosity where n is 4=all,3=no script,2=no info,1=no warnings,0=none', parseInt).option('-w, --wine', 'use Wine to run makenis').option('-x, --strict', 'treat warnings as errors').option('-y, --yaml', 'prints output as YAML').action(function (cmd, filePath, flags) {
+
+  var inputCharset = typeof flags.inputCharset !== 'undefined' && (validCharsets.indexOf(flags.inputCharset) !== -1 || flags.inputCharset.match(/CP\d+/) !== null) ? flags.inputCharset : '';
+  var noCD = typeof flags.nocd === 'undefined' ? false : true;
+  var noConfig = typeof flags.noconfig === 'undefined' ? false : true;
+  var pause = typeof flags.pause === 'undefined' ? false : true;
+  var ppo = typeof flags.ppo === 'undefined' ? false : true;
+  var json = typeof flags.json === 'undefined' ? false : true;
+  var safePPO = typeof flags.safePpo === 'undefined' ? false : true;
+  var strict = typeof flags.strict === 'undefined' ? false : true;
+  var verbose = flags.verbose >= 0 && flags.verbose <= 4 ? flags.verbose : null;
+  var wine = typeof flags.wine === 'undefined' ? false : true;
+  var yaml = typeof flags.yaml === 'undefined' ? false : true;
+
+  var target = null;
+  if (yaml === true && json === false) {
+    target = 'yaml';
+  } else if (json === true && yaml === false) {
+    target = 'json';
+  }
+
+  var options = {
+    'inputCharset': inputCharset,
+    'json': json,
+    'noCD': noCD,
+    'noConfig': noConfig,
+    'pause': pause,
+    'ppo': ppo,
+    'safePPO': safePPO,
+    'strict': strict,
+    'target': target,
+    'verbose': verbose,
+    'wine': wine,
+    'yaml': yaml
+  };
+
+  switch (cmd) {
+    case 'build':
+    case 'c':
+    case 'compile':
+    case 'm':
+    case 'make':
+      compile(filePath, options);
+      break;
+    case 'f':
+    case 'flags':
+    case 'hdrinfo':
+    case 'i':
+    case 'info':
+      hdrinfo(options);
+      break;
+    case 'v':
+    case 'version':
+      version(options);
+      break;
+    case 'h':
+    case 'cmdhelp':
+    case 'help':
+      cmdhelp(filePath, options);
+      break;
+    default:
+      program.help();
+      break;
+  }
+}).parse(process.argv);
+
+if (program.args.length === 0) program.help();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,11 @@
-import * as makensis from 'makensis';
+#!/usr/bin/env node
+
+const makensis = require('makensis');
+const meta = require('../package.json');
+const program = require('commander');
 const YAML = require('yamljs');
 
+// Functions
 const compile = (filePath, options = null) => {
   options || (options = {});
 
@@ -187,9 +192,96 @@ const isInteger = (x) => {
   return x % 2 === 0;
 };
 
-export {
-  cmdhelp,
-  compile,
-  hdrinfo,
-  version
-};
+const validCharsets = [
+  'ACP',
+  'OEM',
+  'UTF8',
+  'UTF16BE',
+  'UTF16LE'
+];
+
+// Action
+program
+  .version(meta.version)
+  .description('CLI version of node-makensis')
+  .arguments('<command> [file.nsi]>')
+  .usage('<command> [file.nsi] [options]')
+  .option('-i, --input-charset <string>', 'ACP|OEM|CP#|UTF8|UTF16[LE|BE]')
+  .option('-j, --json', 'prints output as JSON')
+  .option('-p, --pause', 'pauses after execution')
+  .option('-P, --ppo', 'preprocess to stdout/file')
+  .option('-S, --safe-ppo', 'preprocess to stdout/file')
+  .option('-v, --verbose <n>', 'verbosity where n is 4=all,3=no script,2=no info,1=no warnings,0=none', parseInt)
+  .option('-w, --wine', 'use Wine to run makenis')
+  .option('-x, --strict', 'treat warnings as errors')
+  .option('-y, --yaml', 'prints output as YAML')
+  .action(function(cmd, filePath, flags) {
+
+    let inputCharset = (typeof flags.inputCharset !== 'undefined' && (validCharsets.indexOf(flags.inputCharset) !== -1 || flags.inputCharset.match(/CP\d+/) !== null)) ? flags.inputCharset : '';
+    let noCD = (typeof flags.nocd === 'undefined') ? false : true;
+    let noConfig = (typeof flags.noconfig === 'undefined') ? false : true;
+    let pause = (typeof flags.pause === 'undefined') ? false : true;
+    let ppo = (typeof flags.ppo === 'undefined') ? false : true;
+    let json = (typeof flags.json === 'undefined') ? false : true;
+    let safePPO = (typeof flags.safePpo === 'undefined') ? false : true;
+    let strict = (typeof flags.strict === 'undefined') ? false : true;
+    let verbose = (flags.verbose >= 0 && flags.verbose <= 4) ? flags.verbose : null;
+    let wine = (typeof flags.wine === 'undefined') ? false : true;
+    let yaml = (typeof flags.yaml === 'undefined') ? false : true;
+
+    let target = null;
+    if (yaml === true && json === false) {
+      target = 'yaml'
+    } else if (json === true && yaml === false) {
+      target = 'json'
+    }
+
+    const options = {
+      'inputCharset': inputCharset,
+      'json': json,
+      'noCD': noCD,
+      'noConfig': noConfig,
+      'pause': pause,
+      'ppo': ppo,
+      'safePPO': safePPO,
+      'strict': strict,
+      'target': target,
+      'verbose': verbose,
+      'wine': wine,
+      'yaml': yaml
+    };
+
+    switch (cmd) {
+      case 'build':
+      case 'c':
+      case 'compile':
+      case 'm':
+      case 'make':
+        compile(filePath, options);
+        break;
+      case 'f':
+      case 'flags':
+      case 'hdrinfo':
+      case 'i':
+      case 'info':
+        hdrinfo(options);
+        break;
+      case 'v':
+      case 'version':
+        version(options);
+        break;
+      case 'h':
+      case 'cmdhelp':
+      case 'help':
+        cmdhelp(filePath, options);
+        break;
+      default:
+        program.help();
+        break;
+    }
+  })
+.parse(process.argv);
+
+if (program.args.length === 0) program.help();
+
+
