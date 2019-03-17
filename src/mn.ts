5e4632a24355eff9ec9c2c7ac1fb51e8f622fded
idleberg <git@idleberg.com>
2019-03-17 10:25:23 +0100

    rename scripts

---
+++ src/mn.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import * as program from 'commander';
+
+// Action
+program
+  .version(require('../package.json').version)
+  .description('CLI version of node-makensis')
+  .command('hdrinfo', 'Print compilation flags').alias('flags')
+  .command('compile <script>', 'Compiles script(s)', {isDefault: true})
+  .command('version [options]', 'Import repository')
+  .command('cmdhelp [command] [options]', 'Prints out help for single or all commands').alias('help')
+  .command('license [options]', 'Prints license')
+  .command('nsisdir', 'Prints NSIS installation folder').alias('dir')
+  .command('scaffold', 'Creates custom made NSIS script').alias('new')
+  .parse(process.argv);
